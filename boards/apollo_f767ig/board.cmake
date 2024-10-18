7538efabaf256959c411301e8368a25f4a93371d
kabirz <jxwazxzhp@126.com>
2024-10-18 14:31:18 +0800

    zephyr: rtt echo
    Signed-off-by: kabirz <jxwazxzhp@126.com>

--- boards/apollo_f767ig/board.cmake
+++ boards/apollo_f767ig/board.cmake
@@ -1,8 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # keep first
+board_runner_args(probe-rs "--chip" "STM32F767IGTx")
+board_runner_args(pyocd "--target" "STM32F767IGTx")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 # keep first
-include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
