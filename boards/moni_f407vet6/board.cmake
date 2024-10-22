0c8577c28ad27f64d03644986bb025cf34d27b0d
kabirz <jxwazxzhp@126.com>
2024-10-22 10:08:12 +0800

    zephyr: f407 moni update
    Signed-off-by: kabirz <jxwazxzhp@126.com>

--- boards/moni_f407vet6/board.cmake
+++ boards/moni_f407vet6/board.cmake
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
-board_runner_args(jlink "--device=STM32F407VE" "--speed=4000")
+board_runner_args(probe-rs "--chip" "STM32F401VETx")
+board_runner_args(pyocd "--target" "STM32F401VETx")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
